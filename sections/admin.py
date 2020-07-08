2cef8043d62062555e60947af7533cfa9a28c8b9
enoch chejieh <enochchejieh@gmail.com>
2020-07-08 12:18:25 +0100

    setting change

--- sections/admin.py
+++ sections/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
+from django.contrib.sites.models import Site
+
+admin.site.unregister(Site)
+class SiteAdmin(admin.ModelAdmin):
+    fields = ('id', 'name', 'domain')
+    readonly_fields = ('id',)
+    list_display = ('id', 'name', 'domain')
+    list_display_links = ('name',)
+    search_fields = ('name', 'domain')
+
+
+
 class DescriptionInline(admin.TabularInline):
 	model=Description
 	extra=5
@@ -14,3 +26,4 @@ admin.site.register(Cart)
 admin.site.register(Orderitem)
 admin.site.register(Customer)
 admin.site.register(Shipping)
+admin.site.register(Site, SiteAdmin)
